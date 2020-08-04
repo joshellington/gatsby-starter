d82541eb04b1e013f0feea3d6a796c13b55a6746
Joshua Ellington <hello@joshellington.com>
2020-08-04 10:18:10 -0700

    Base starter setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,41 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+console.log(`!!! ACTIVE_ENV: ${activeEnv}`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
+const targetAddress = new URL(process.env.TARGET_ADDRESS || `https://joshellington.com`);
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Josh Ellington`,
+    description: `joshellington.com`,
+    author: `@joshellington`,
+    siteUrl: `https://joshellington.com`,
+  },
+  plugins: [
+    { resolve: `gatsby-plugin-react-helmet` },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "fonts",
+        path: `${__dirname}/src/fonts/`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    { resolve: "gatsby-plugin-sass" },
+    {
+      resolve: "gatsby-plugin-canonical-urls",
+      options: {
+        siteUrl: targetAddress.href.slice(0, -1)
+      }
+    },
+  ]
 }
